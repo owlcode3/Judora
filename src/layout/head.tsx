b8d0e3c939122f7d16d2296612ac4be63c4cadb6
owlCode <akoladeo890@yahoo.com>
2023-10-20 15:47:01 +0100

    made some changes to the styles in head.tsx file

--- src/layout/head.tsx
+++ src/layout/head.tsx
@@ -4,8 +4,8 @@ import Hero from "../components/hero"
 
 function Head() {
   return (
-    <div className='bg-[url("/bg-img.svg")] bg-cover h-[48rem]'>
-      <div className="w-full px-32 pt-8">
+    <div className='bg-[url("/bg-img.svg")] min-h-[48rem] max-h-[48rem] bg-cover bg-center bg-no-repeat'>
+      <div className="w-full px-32 py-8">
         <Header />
         <Hero />
       </div>
